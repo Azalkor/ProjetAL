24b33f046d0d4aeef54ebf7fef596497fff1f7af
jsabourin001 <jeremysabourin24@gmail.com>
2017-03-20 15:54:32 +0100

    Formes gérées

--- src/model/Polygone.java
+++ src/model/Polygone.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Polygone extends AbstractShape{
-	private float 
+	private int nbCotes;
+	private float longueurCotes;
+	
+	public int getNbCotes() {
+		return nbCotes;
+	}
+	public void setNbCotes(int nbCotes) {
+		this.nbCotes = nbCotes;
+	}
+	
+	public float getLongueurCotes() {
+		return longueurCotes;
+	}
+	public void setLongueurCotes(float longueurCotes) {
+		this.longueurCotes = longueurCotes;
+	}
+	
+	public Polygone(){}
 }
--- src/model/Rectangle.java
+++ src/model/Rectangle.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Rectangle extends AbstractShape{
+	private float largeur;
+	private float hauteur;
+	
+	public float getLargeur() {
+		return largeur;
+	}
+	public void setLargeur(float largeur) {
+		this.largeur = largeur;
+	}
 
+	public float getHauteur() {
+		return hauteur;
+	}
+	public void setHauteur(float hauteur) {
+		this.hauteur = hauteur;
+	}
+
+	public Rectangle(){}
+	
+	public void arrondirBords(){}
 }
