9f3554998ab9f3712c54f6ec67cb8f39fc6548bb
Clement Nerestan <clenerestan@emi.u-bordeaux1.fr>
2017-03-27 14:13:39 +0200

    MDR le main

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main  extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("ProjetAL");
+	    Pane myPane = (Pane)FXMLLoader.load(getClass().getResource("View/FenetreProjet.fxml"));
+	    Scene myScene = new Scene(myPane);
+	    primaryStage.setScene(myScene);
+	    primaryStage.show();
+		
+	}
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
