79783e67d724aedabb013e6b4218ecff9f56a1c8
Azalkor <jeremysabourin24@gmail.com>
2017-05-05 04:08:44 +0200

    clic droit

--- src/controller/CreateGroup.java
+++ src/controller/CreateGroup.java
@@ -7,6 +7,7 @@ import javafx.scene.SnapshotParameters;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
@@ -18,80 +19,85 @@ import model.Shape;
 import model.ShapeGroup;
 
 public class CreateGroup {
-	
+
 	static Rectangle selectZone;
 	ShapeGroup selectedShapes = new ShapeGroup();
-	
-	public CreateGroup(Model m, Pane p, Pane sPane, double yOffset){
-		
+
+	public CreateGroup(Model m, Pane p, Pane sPane, double yOffset) {
+
 		p.setOnMousePressed(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent event){
-				if(selectZone!=null){
-					if (!selectZone.contains(new Point2D(event.getX(),event.getY()))){
-						p.getChildren().remove(selectZone);
-						selectedShapes.getShapes().clear();
+			public void handle(MouseEvent event) {
+				if (event.getButton() == MouseButton.PRIMARY) {
+					if (selectZone != null) {
+						if (!selectZone.contains(new Point2D(event.getX(), event.getY()))) {
+							p.getChildren().remove(selectZone);
+							selectedShapes.getShapes().clear();
+						} else
+							return;
 					}
-					else
-						return;
-				}
-				if(!(Controller.state==5)){
-					Controller.state=3;
-					selectZone = new Rectangle(event.getX(),event.getY(),0,0);
-		            selectZone.setFill(Color.TRANSPARENT);
-		            selectZone.setStroke(Color.TRANSPARENT);
-		            selectZone.setStrokeWidth(1);
-		            selectZone.toFront();
-		            p.getChildren().addAll(selectZone);
+					if (!(Controller.state == 5)) {
+						Controller.state = 3;
+						selectZone = new Rectangle(event.getX(), event.getY(), 0, 0);
+						selectZone.setFill(Color.TRANSPARENT);
+						selectZone.setStroke(Color.TRANSPARENT);
+						selectZone.setStrokeWidth(1);
+						selectZone.toFront();
+						p.getChildren().addAll(selectZone);
+					}
 				}
 			}
 		});
 		p.setOnMouseDragged(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent event) {
-				if(!(Controller.state==5))
-					if(Controller.state==3){
-						System.out.println("getX : "+event.getX());
-						selectZone.setWidth(event.getX()-selectZone.getX());
-						selectZone.setHeight(event.getY()-selectZone.getY());
-			            selectZone.setStroke(Color.BLACK);
+				if (!(Controller.state == 5))
+					if (Controller.state == 3) {
+						selectZone.setWidth(event.getX() - selectZone.getX());
+						selectZone.setHeight(event.getY() - selectZone.getY());
+						selectZone.setStroke(Color.BLACK);
 					}
 			}
-			
+
 		});
 		p.setOnMouseReleased(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent event) {
-				Controller.state=4;
-				for (Shape s : m.getGroup().getShapes()) {
-					if(selectZone.contains(s.getCentre())){
-						selectedShapes.addShape(s);
+				if (event.getButton() == MouseButton.PRIMARY) {
+					Controller.state = 4;
+					for (Shape s : m.getGroup().getShapes()) {
+						if (selectZone.contains(s.getCentre())) {
+							selectedShapes.addShape(s);
+						}
 					}
-				}
-				if(!selectedShapes.getShapes().isEmpty()){
-					if(selectedShapes.getShapes().size()>1)
-						m.getGroup().addShape(selectedShapes);
-					else
-						m.getGroup().addShape(selectedShapes.getShapes().get(0));
-				}
-				selectZone.setOnDragDetected(new EventHandler<MouseEvent>() {
-					public void handle(MouseEvent event) {
-						Dragboard db = selectZone.startDragAndDrop(TransferMode.ANY);
-						ClipboardContent content = new ClipboardContent();
-						content.putString("");
-						db.setContent(content);
-						WritableImage wi = new WritableImage((int)selectZone.getWidth(), (int)selectZone.getHeight());
-						WritableImage wi2 = new WritableImage((int)selectZone.getWidth(), (int)selectZone.getHeight());
-						SnapshotParameters sp = new SnapshotParameters();
-						sp.setViewport(new Rectangle2D(selectZone.getX()+sPane.getPrefWidth(),selectZone.getY()+yOffset,1000,1000));
-						p.snapshot(sp, wi2);
-						db.setDragView(wi2);
-						sp.setFill(Color.TRANSPARENT);
-						p.snapshot(sp, wi);
-						Rectangle rTest = new Rectangle(wi.getWidth(),wi.getHeight());
-						ImagePattern ip = new ImagePattern(wi);
-						rTest.setFill(ip);
-						sPane.getChildren().add(rTest);
-						event.consume();
+					if (!selectedShapes.getShapes().isEmpty()) {
+						if (selectedShapes.getShapes().size() > 1)
+							m.getGroup().addShape(selectedShapes);
+						else
+							m.getGroup().addShape(selectedShapes.getShapes().get(0));
 					}
-				});
+					selectZone.setOnDragDetected(new EventHandler<MouseEvent>() {
+						public void handle(MouseEvent event) {
+							Dragboard db = selectZone.startDragAndDrop(TransferMode.ANY);
+							ClipboardContent content = new ClipboardContent();
+							content.putString("");
+							db.setContent(content);
+							WritableImage wi = new WritableImage((int) selectZone.getWidth(),
+									(int) selectZone.getHeight());
+							WritableImage wi2 = new WritableImage((int) selectZone.getWidth(),
+									(int) selectZone.getHeight());
+							SnapshotParameters sp = new SnapshotParameters();
+							sp.setViewport(new Rectangle2D(selectZone.getX() + sPane.getPrefWidth(),
+									selectZone.getY() + yOffset, 1000, 1000));
+							p.snapshot(sp, wi2);
+							db.setDragView(wi2);
+							sp.setFill(Color.TRANSPARENT);
+							p.snapshot(sp, wi);
+							Rectangle rTest = new Rectangle(wi.getWidth(), wi.getHeight());
+							ImagePattern ip = new ImagePattern(wi);
+							rTest.setFill(ip);
+							sPane.getChildren().add(rTest);
+							event.consume();
+						}
+					});
+				}
 				event.consume();
 			}
 		});
--- src/controller/DragAndDrop.java
+++ src/controller/DragAndDrop.java
@@ -1,11 +1,16 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
@@ -45,40 +50,89 @@ public class DragAndDrop {
 				if (Controller.state != 4) {
 					Controller.state = 2;
 					Point2D dropPos = new Point2D(event.getX(), event.getY());
-					
+
 					if (event.getGestureSource() instanceof Rectangle) {
 						Rectangle r = (Rectangle) event.getGestureSource();
 						m.DrawRect((float) r.getWidth(), (float) r.getHeight(), dropPos, (Color) r.getFill());
-						Rectangle newShape = new Rectangle(dropPos.getX(), dropPos.getY(), r.getWidth(),r.getHeight());
+						Rectangle newShape = new Rectangle(dropPos.getX(), dropPos.getY(), r.getWidth(), r.getHeight());
 						newShape.setFill(r.getFill());
-							newShape.setOnMouseDragged(new EventHandler<MouseEvent>() {
-								public void handle(MouseEvent event) {
-									Controller.state = 5;
-									newShape.setX(event.getX());
-									newShape.setY(event.getY());
+						newShape.setOnMouseDragged(new EventHandler<MouseEvent>() {
+							public void handle(MouseEvent event) {
+								Controller.state = 5;
+								newShape.setX(event.getX());
+								newShape.setY(event.getY());
+								event.consume();
+							}
+						});
+						newShape.setOnMousePressed(new EventHandler<MouseEvent>() {
+							public void handle(MouseEvent event) {
+								if (event.getButton() == MouseButton.SECONDARY) {
+									System.out.println("yolo");
+									TextField textField = new TextField("Type Something");
+									final ContextMenu contextMenu = new ContextMenu();
+									MenuItem couleur = new MenuItem("Couleur");
+									MenuItem arrondi = new MenuItem("Arrondir bords");
+									MenuItem rotation = new MenuItem("Rotation");
+									MenuItem largeur = new MenuItem("Largeur");
+									MenuItem hauteur = new MenuItem("Hauteur");
+									contextMenu.getItems().addAll(couleur, arrondi, rotation, largeur, hauteur);
+									couleur.setOnAction(new EventHandler<ActionEvent>() {
+										@Override
+										public void handle(ActionEvent event) {
+											System.out.println("Cut...");
+										}
+									});
+									// ...
+									textField.setContextMenu(contextMenu);
+									contextMenu.show(newShape, event.getScreenX(), event.getScreenY());
 								}
-							});
+								event.consume();
+							}
+						});
 						dropPane.getChildren().add(newShape);
 					} else if (event.getGestureSource() instanceof Polygon) {
 						Polygon p = (Polygon) event.getGestureSource();
 						model.Polygone tmp = m.DrawPoly(p.getPoints().size() / 2,
-											(int) new Point2D(p.getPoints().get(0), p.getPoints().get(1))
-											.distance(p.getPoints().get(2), p.getPoints().get(3)),
-											dropPos, (Color) p.getFill());
-						System.out.println(tmp.getPoints());
+								(int) new Point2D(p.getPoints().get(0), p.getPoints().get(1))
+										.distance(p.getPoints().get(2), p.getPoints().get(3)),
+								dropPos, (Color) p.getFill());
 						Polygon newShape = new Polygon(tmp.getPoints());
 						newShape.setFill(p.getFill());
-							newShape.setOnMouseDragged(new EventHandler<MouseEvent>() {
-								public void handle(MouseEvent event) {
-									Controller.state = 5;
-									Polygone tmp = new Polygone(newShape.getPoints().size() / 2,
-											(int) new Point2D(newShape.getPoints().get(0), newShape.getPoints().get(1))
-											.distance(newShape.getPoints().get(2), newShape.getPoints().get(3)),
-											new Point2D(event.getX(),event.getY()), (Color) p.getFill());
-									Double[] tab = new Double[tmp.getPoints().length];
-									for(int i=0;i<tmp.getPoints().length;i++)
-										tab[i]=tmp.getPoints()[i];
-									newShape.getPoints().setAll(tab);
+						newShape.setOnMouseDragged(new EventHandler<MouseEvent>() {
+							public void handle(MouseEvent event) {
+								Controller.state = 5;
+								Polygone tmp = new Polygone(newShape.getPoints().size() / 2,
+										(int) new Point2D(newShape.getPoints().get(0), newShape.getPoints().get(1))
+												.distance(newShape.getPoints().get(2), newShape.getPoints().get(3)),
+										new Point2D(event.getX(), event.getY()), (Color) p.getFill());
+								Double[] tab = new Double[tmp.getPoints().length];
+								for (int i = 0; i < tmp.getPoints().length; i++)
+									tab[i] = tmp.getPoints()[i];
+								newShape.getPoints().setAll(tab);
+							}
+						});
+						newShape.setOnMousePressed(new EventHandler<MouseEvent>() {
+							public void handle(MouseEvent event) {
+								if (event.getButton() == MouseButton.SECONDARY) {
+									System.out.println("yolo");
+									TextField textField = new TextField("Type Something");
+									final ContextMenu contextMenu = new ContextMenu();
+									MenuItem couleur = new MenuItem("Couleur");
+									MenuItem rotation = new MenuItem("Rotation");
+									MenuItem nbCotes = new MenuItem("Nombre de côtés");
+									MenuItem longueurCotes = new MenuItem("Longueur des côtés");
+									contextMenu.getItems().addAll(couleur, rotation, nbCotes, longueurCotes);
+									couleur.setOnAction(new EventHandler<ActionEvent>() {
+										@Override
+										public void handle(ActionEvent event) {
+											System.out.println("Cut...");
+										}
+									});
+									// ...
+									textField.setContextMenu(contextMenu);
+									contextMenu.show(newShape, event.getScreenX(), event.getScreenY());
+								}
+								event.consume();
 							}
 						});
 						dropPane.getChildren().add(newShape);
